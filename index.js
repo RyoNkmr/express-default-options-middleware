a1f97c4c5c0317e196c093eaf8e20e3a41e05990
lyan- <ryonakamuraryo@gmail.com>
2017-01-24 12:32:13 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var _ = require('lodash');
+
+/**
+ * Express Default Options Middleware
+ *
+ * the middleware, merge default options to request query
+ *
+ * @param {object} [options]
+ * @return {Function} middleware
+ * @public
+ */
+
+module.exports = function(options) {
+  var opts = options || {};
+
+  return function expressDefaultOptionsMiddleware(req, res, next) {
+    var query = _.defaultsDeep(req.query, opts);
+    console.log(query);
+    if(Object.keys(query).length === 0) {
+      return next();
+    }
+    req.query = query;
+    return next();
+  };
+};
